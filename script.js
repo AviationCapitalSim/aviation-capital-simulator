b27ee2c28da04a8f404bff476e0910ef832f2853
AviationCapitalSim <aviationcapitalsim@gmail.com>
2025-10-31 15:51:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function startTrial() {
   alert("Welcome Captain! You’ve started your free 7-day trial with 700 credits.");
 }
+// Efecto premium: cambiar header al hacer scroll
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".acs-header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
