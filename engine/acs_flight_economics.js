3e8cc3da0846ddbb44f1cecf66344812eff7f682
AviationCapitalSim <aviationcapitalsim@gmail.com>
2026-01-16 20:47:07 +0600

    Update acs_flight_economics.js
    return {
      flightId: d.flightId,
      aircraftId: d.aircraftId,
      origin: d.origin,
      destination: d.destination,
      distanceNM,
    
      // Pax
      pax,
      loadFactor,
    
      // Revenue
      revenue,
    
      // Costs (desglosados)
      fuelCost: costFuel,
      slotCost: costSlots,
      handlingCost,
    
      // Totals
      costTotal: costFuel + costSlots + handlingCost,
      profit: revenue - (costFuel + costSlots + handlingCost),
    
      // Metrics
      paxPerNM,
      revPerNM,
      costPerNM:
        distanceNM > 0
          ? (costFuel + costSlots + handlingCost) / distanceNM
          : 0,
      costPerPax:
        pax > 0
          ? (costFuel + costSlots + handlingCost) / pax
          : 0,
    
      // Meta
      year,
      arrAbsMin: d.arrAbsMin ?? null,
      ts: Date.now()
    };

--- engine/acs_flight_economics.js
+++ engine/acs_flight_economics.js
@@ -428,6 +428,7 @@ const profit = revenue - costTotal;
   /* ============================================================
      📦 FINAL ECONOMICS OBJECT
      ============================================================ */
+   
  return {
   flightId: d.flightId,
   aircraftId: d.aircraftId,
@@ -435,9 +436,11 @@ const profit = revenue - costTotal;
   destination: d.destination,
   distanceNM,
 
+  // Pax
   pax,
   loadFactor,
 
+  // Revenue
   revenue,
 
   // Costs (desglosados)
@@ -452,8 +455,14 @@ const profit = revenue - costTotal;
   // Metrics
   paxPerNM,
   revPerNM,
-  costPerNM: distanceNM > 0 ? (costFuel + costSlots + handlingCost) / distanceNM : 0,
-  costPerPax: pax > 0 ? (costFuel + costSlots + handlingCost) / pax : 0,
+  costPerNM:
+    distanceNM > 0
+      ? (costFuel + costSlots + handlingCost) / distanceNM
+      : 0,
+  costPerPax:
+    pax > 0
+      ? (costFuel + costSlots + handlingCost) / pax
+      : 0,
 
   // Meta
   year,
